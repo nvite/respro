e2b931b8fca88ffa2d29b220bf9feeca29bd56f7
Evan Dudla <evan@dud.la>
2015-02-03 17:17:40 -0500

    Make request and forward req/res headers

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
+/*jshint expr:true, latedef:false, laxcomma:true */
+
+var request = require('request')
+  , url = require('url')
+  , extend = require('util-extend');
 
 /**
  * @file Resource proxy middleware for Connect/Express.
  *
- * @param   {String} resourceUrl - The destination resource to fetch.
- * @param   {Object} resHeaders  - A map of response headers to send with our
+ * @param   {Mixed}  resourceUrl - The destination resource to fetch. This can
+ *                                 be a String, or an Object formatted the same
+ *                                 way a return from `url.parse()` might be.
+ * @param   {Mixed}  resHeaders  - A map of response headers to send with our
  *                                 response. Passing a String is shorthand for
  *                                 specifying the Content-Type header directly.
  * @param   {Object} reqHeaders  - A map of request headers to send along with
@@ -13,17 +20,77 @@
  * @returns {Function}           - A Connect/Express compatible middleware Fn.
  */
 module.exports = function (resourceUrl, resHeaders, reqHeaders) {
-  var contentType;
+
+  if (!resourceUrl) {
+    throw new TypeError('You must provide a resourceUrl to proxy to.');
+  }
+  if ('string' === typeof resourceUrl) {
+    resourceUrl = url.parse(resourceUrl);
+  }
 
   // If we pass in a string for the `resHeaders` argument, treat it as a short-
   // hand for specifying the Content-Type header.
   if ('string' === typeof resHeaders) {
-    contentType = resHeaders;
+    resHeaders = { 'Content-Type': resHeaders };
   }
 
-  if (!contentType) {
-    contentType = resHeaders['Content-Type'] || 'application/octet-stream';
-  }
+  resHeaders && (resHeaders = getComparableObj(resHeaders)) || (resHeaders = {});
+  reqHeaders && (reqHeaders = getComparableObj(reqHeaders)) || (reqHeaders = {});
 
-  console.log('Got content-type', contentType);
+  return function (req, res, next) {
+    req.reqHeaders = reqHeaders;
+    req.resHeaders = resHeaders;
+
+    var options = {
+      url: url.format(resourceUrl),
+      headers: extend(getComparableObj(req.headers), req.reqHeaders)
+    };
+
+    // We must do this to avoid TLS errors with hostname/ip mismatches.
+    delete options.headers.host;
+
+    request(options)
+      .on('error', RequestErrorHandler.call(null, req, res, next))
+      .on('response', RequestResponseHandler.call(null, req, res, next));
+  };
 };
+
+/**
+ * Handle errors from our resource request.
+ */
+function RequestErrorHandler(req, res, next) {
+  return function (err) {
+    next(err);
+  };
+}
+
+/**
+ * Handle a successful response from our resource request.
+ */
+function RequestResponseHandler(req, res, next) {
+  return function (response, body) {
+
+    // Merge and set our user-defined headers and the headers we got from our
+    // resource request.
+    var responseHeaders = extend(getComparableObj(response.headers), req.resHeaders);
+    res.set(responseHeaders);
+
+    res.send(body);
+  };
+}
+
+/**
+ * Returns a new copy of an object with all the keys lowercase, primarily
+ * used for comparison/extension of objects.
+ */
+function getComparableObj(obj) {
+  var o = {};
+
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      o[prop.toLowerCase()] = obj[prop];
+    }
+  }
+
+  return o;
+}
